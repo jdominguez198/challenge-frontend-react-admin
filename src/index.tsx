7d80236a458fca3b39c0d780572175b841bd3aa9
Jesus Dominguez <j.dominguez@kodeline.com>
2021-07-12 06:08:38 +0200

    Typescript usage; Install base libraries; Change App name

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <p>Welcome!</p>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
